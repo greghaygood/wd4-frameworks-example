5f5b7a5fa5715118bb22feee927a3d1ed45bcdd9
Greg Haygood <ghaygood@gmail.com>
2016-07-29 16:31:51 -0400

    add browser sync capability

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
 var gutil = require('gulp-util');
+var browserSync = require('browser-sync').create();
 
 var onError = function(err) {
 	console.log('An error occurred:', gutil.colors.magenta(err.message));
@@ -9,11 +10,26 @@ var onError = function(err) {
 	this.emit('end');
 };
 
+gulp.task('browser-sync', function() {
+	browserSync.init({
+		server: {
+			baseDir: "./"
+		}
+	});
+
+});
+
 gulp.task('scss', function() {
 	return gulp.src('scss/**/*.scss')
 	.pipe(plumber({errorHandler: onError}))
 	.pipe(sass())
-	.pipe(gulp.dest('css'));
+	.pipe(gulp.dest('css'))
+	.pipe(browserSync.stream());
+});
+
+gulp.task('server', ['scss', 'browser-sync'], function() {
+	gulp.watch('scss/**/*.scss', ['scss']);
+	gulp.watch(['**/*.html', 'css/**/*.css']).on('change', browserSync.reload);
 });
 
 gulp.task('watch', function() {
